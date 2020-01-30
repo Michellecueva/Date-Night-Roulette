de9a5b25d157917080f6cdc71a5ddcb4bcbd14a0
phoenixmcknight <phoenixmcknight@gmail.com>
2020-01-30 17:05:21 -0500

    Creates extensions to check valid emails and passwords

--- Date-Night/Extensions/Extensions.swift
+++ Date-Night/Extensions/Extensions.swift
@@ -131,13 +131,26 @@ extension String {
         return formatDate
     }
     
-    static func isEmailValid(_ email : String) -> Bool {
-        let emailTest = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
-        return emailTest.evaluate(with: email)
-    }
-    
-    static func isPasswordValid(_ password : String) -> Bool {
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
-        return passwordTest.evaluate(with: password)
-    }
+        var isValidEmail: Bool {
+           
+           // this pattern requires that an email use the following format:
+           // [something]@[some domain].[some tld]
+           let validEmailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+           
+           let emailPredicate = NSPredicate(format:"SELF MATCHES %@", validEmailRegEx)
+           return emailPredicate.evaluate(with: self)
+       }
+     
+       
+       var isValidPassword: Bool {
+           
+           //this pattern requires that a password has at least one capital letter, one number, one lower case letter, and is at least 8 characters long
+           //let validPasswordRegEx =  "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}"
+           
+           //this pattern requires that a password be at least 8 characters long
+           let validPasswordRegEx =  "[A-Z0-9a-z!@#$&*.-]{8,}"
+           let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", validPasswordRegEx)
+           return passwordPredicate.evaluate(with: self)
+       }
+
 }
