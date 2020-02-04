64327a40d484e0360854361af3cfd3b156280d66
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2020-02-04 15:58:47 -0500

    Add API Client to retrieve data

---
+++ Date-Night/Networking/SeatGeekAPIClient.swift
@@ -0,0 +1,40 @@
+//
+//  SeatGeekAPIClient.swift
+//  CapstoneDraft2
+//
+//  Created by Krystal Campbell on 1/30/20.
+//  Copyright © 2020 Krystal Campbell. All rights reserved.
+//
+
+import Foundation
+
+
+struct SeatGeekAPIClient{
+    private init() {}
+    static let shared = SeatGeekAPIClient()
+    
+    
+    func getEventsFrom(category: String, completionHandler: @escaping (Result <[Event], AppError>) ->()){
+        let urlStr = "https://api.seatgeek.com/2/events?type=\(category)&geoip=true&client_id=\(Secrets.seatKey)"
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(AppError.badURL))
+            return
+        }
+        NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(.other(rawError: error)))
+            case .success(let data):
+                do{
+                    let catArray = try Event.getSeatGeekData(data: data)
+                    completionHandler(.success(catArray ?? []))
+                    
+                } catch {
+                    completionHandler(.failure(AppError.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+    }
+}
+        
