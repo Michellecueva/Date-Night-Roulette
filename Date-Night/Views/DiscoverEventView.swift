a270163c4d3b08c72d82c6897aa769e8aa94892e
Kyang646 <kimballyang@pursuit>
2020-02-06 15:15:38 -0500

    creates DiscoverEventView

---
+++ Date-Night/Views/DiscoverEventView.swift
@@ -0,0 +1,21 @@
+//
+//  DiscoverEventView.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 2/6/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverEventView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
