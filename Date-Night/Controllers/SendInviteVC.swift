2b3cae55640fb887fee12d2ff0935b41c7b5daba
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-06 16:33:35 -0500

    creates sendInviteVC

---
+++ Date-Night/Controllers/SendInviteVC.swift
@@ -0,0 +1,30 @@
+//
+//  SendInviteVC.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/6/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class SendInviteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
