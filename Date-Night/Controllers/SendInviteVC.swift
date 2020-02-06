8ce96181a0efb1f386b9f369b29500b709a4314c
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-06 17:38:05 -0500

    connects SendInviteView objects  to SendInviteVC invite functions

--- Date-Night/Controllers/SendInviteVC.swift
+++ Date-Night/Controllers/SendInviteVC.swift
@@ -7,24 +7,50 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SendInviteVC: UIViewController {
 
+    let sendInviteView = SendInviteView()
+    
+    private var currentUserEmail:String {
+           if let user = Auth.auth().currentUser?.email {
+               return user
+           } else {
+               return "Invalid Email"
+           }
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func addObjcFunctions() {
+        sendInviteView.enterButton.addTarget(self, action: #selector(sendInvite), for: .touchUpInside)
     }
-    */
-
+    
+  @objc private func sendInvite() {
+          
+    
+    guard let recipient = sendInviteView.emailField.text else {
+            //add alert
+            print("could not invite user")
+            return
+        }
+        
+        let invite = Invites(from: currentUserEmail, to: recipient, invitationStatus: .pending)
+           
+           FirestoreService.manager.sendInvite(invite: invite) { [weak self] (result) in
+               switch result {
+               case .failure(let error):
+                   print(error)
+                   
+               case .success():
+                self?.showAlert(title: "Success!", message: "Invited \(recipient)")
+                   print("Invite was succesfully sent")
+               }
+           }
+       }
 }
