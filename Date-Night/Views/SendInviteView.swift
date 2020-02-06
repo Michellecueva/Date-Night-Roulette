1359fa42c1e611a845847e089c86bd913ad5a9cf
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-06 16:32:47 -0500

    creates SendInviteView

---
+++ Date-Night/Views/SendInviteView.swift
@@ -0,0 +1,15 @@
+//
+//  SendInviteView.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/6/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class SendInviteView: UIView {
+
+   
+
+}
