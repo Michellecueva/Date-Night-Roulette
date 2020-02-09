aab33fc9992cbb3bcb78893dd478807ca672487e
Michelle <Michelle.cueva@gmail.com>
2020-02-09 18:10:30 -0500

    Creates a diffable Data Source

--- Date-Night/Controllers/InvitesPendingVC.swift
+++ Date-Night/Controllers/InvitesPendingVC.swift
@@ -1,30 +1,47 @@
-//
-//  InvitesPendingVC.swift
-//  Date-Night
-//
-//  Created by Michelle Cueva on 2/9/20.
-//  Copyright © 2020 Michelle Cueva. All rights reserved.
-//
 
 import UIKit
 
 class InvitesPendingVC: UIViewController {
+    
+    var invites = [
+          Invites(from: "Test", to: "Michelle", invitationStatus: invitationStatus.pending),
+          Invites(from: "Test 2", to: "Steven", invitationStatus: invitationStatus.pending),
+          Invites(from: "Test 3", to: "Person", invitationStatus:invitationStatus.pending)
+      ]
+    
+    let invitesPendingView = InvitesPendingView()
+    
+    private var dataSource: UITableViewDiffableDataSource<Section, Invites>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(FirebaseAuthService.manager.currentUser?.email)
+        view.addSubview(invitesPendingView)
+        configureDataSource()
+        createSnapshot(from: invites)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, Invites>(tableView: invitesPendingView.invitesPendingTableView, cellProvider: { (tableView, indexPath, Invite) -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "invitesCell", for: indexPath)
+            cell.textLabel?.text = Invite.to
+            cell.textLabel?.textColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+            cell.backgroundColor = .clear
+            return cell
+        })
     }
-    */
+    
+    private func createSnapshot(from invites: [Invites]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Invites>()
+        
+        snapshot.appendSections([.main])
+        snapshot.appendItems(invites)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
 
+extension InvitesPendingVC {
+    fileprivate enum Section {
+        case main
+    }
 }
