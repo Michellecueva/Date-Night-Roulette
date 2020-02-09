e0d9cb7c7d86e0d2b2bf4f1e3067a57c7aadc687
Michelle <Michelle.cueva@gmail.com>
2020-02-09 17:54:04 -0500

    Creates UITableViewCell

---
+++ Date-Night/Views/InvitesCell.swift
@@ -0,0 +1,24 @@
+//
+//  InvitesCell.swift
+//  Date-Night
+//
+//  Created by Michelle Cueva on 2/9/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class InvitesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
