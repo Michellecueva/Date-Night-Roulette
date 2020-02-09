963f9ab5a21276a46bd59e0db2a05f3cad435009
Michelle <Michelle.cueva@gmail.com>
2020-02-09 17:56:58 -0500

    Creates name label in Invites Cell

--- Date-Night/Views/InvitesCell.swift
+++ Date-Night/Views/InvitesCell.swift
@@ -1,24 +1,44 @@
-//
-//  InvitesCell.swift
-//  Date-Night
-//
-//  Created by Michelle Cueva on 2/9/20.
-//  Copyright © 2020 Michelle Cueva. All rights reserved.
-//
 
 import UIKit
 
-class InvitesCell: UITableViewCell {
+class InvitesPendingCell: UITableViewCell {
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        setConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    private func addSubviews() {
+        self.contentView.addSubview(nameLabel)
+    }
+    
+    private func setConstraints() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            nameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            nameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.8)
+        ])
     }
 
 }
