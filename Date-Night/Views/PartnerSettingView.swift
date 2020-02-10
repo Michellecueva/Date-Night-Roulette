d9606fe4b5385b02885798dd836fe19014a6aa5f
Kyang646 <kimballyang@pursuit>
2020-02-10 12:08:36 -0500

    first commit

---
+++ Date-Night/Views/PartnerSettingView.swift
@@ -0,0 +1,21 @@
+//
+//  PartnerSettingView.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 2/10/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class PartnerSettingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
