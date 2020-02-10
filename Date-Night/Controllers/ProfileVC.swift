a5fb362d17dcf0350e80d208816f1d8c3e098042
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-10 17:05:00 -0500

    creates profileVC

--- Date-Night/Controllers/ProfileVC.swift
+++ Date-Night/Controllers/ProfileVC.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2020 Michelle Cueva. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProfileSettingVC: UIViewController {
+
+    var profileSetting = ProfileSettingView()
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.addSubview(profileSetting)
+    }
+}
