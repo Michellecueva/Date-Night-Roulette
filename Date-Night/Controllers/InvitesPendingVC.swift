33540bdbea2dd70cb64f6935788540f8a075297c
Michelle <Michelle.cueva@gmail.com>
2020-02-09 21:11:12 -0500

    Adds snapshot listener to check invites and populates tableview with firebase data

--- Date-Night/Controllers/InvitesPendingVC.swift
+++ Date-Night/Controllers/InvitesPendingVC.swift
@@ -1,30 +1,50 @@
-
+import FirebaseAuth
+import FirebaseFirestore
 import UIKit
 
 class InvitesPendingVC: UIViewController {
     
-    var invites = [
-          Invites(from: "Test", to: "Michelle", invitationStatus: invitationStatus.pending),
-          Invites(from: "Test 2", to: "Steven", invitationStatus: invitationStatus.pending),
-          Invites(from: "Test 3", to: "Person", invitationStatus:invitationStatus.pending)
-      ]
+    var invites: [Invites] = [] {
+        didSet {
+            createSnapshot(from: invites)
+        }
+    }
     
     let invitesPendingView = InvitesPendingView()
     
     private var dataSource: UITableViewDiffableDataSource<Section, Invites>!
-
+    
+    private var inviteListener: ListenerRegistration?
+    
+    private let db = Firestore.firestore()
+    
+    private var currentUserEmail:String {
+        if let user = Auth.auth().currentUser?.email {
+            return user
+        } else {
+            return "Invalid Email"
+        }
+    }
+    
+    private var collectionReference:CollectionReference {
+        return db.collection("invites")
+    }
+    
+    deinit {
+        inviteListener?.remove()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(FirebaseAuthService.manager.currentUser?.email)
         view.addSubview(invitesPendingView)
         configureDataSource()
-        createSnapshot(from: invites)
+        addListener()
     }
     
     private func configureDataSource() {
         dataSource = UITableViewDiffableDataSource<Section, Invites>(tableView: invitesPendingView.invitesPendingTableView, cellProvider: { (tableView, indexPath, Invite) -> UITableViewCell? in
             let cell = tableView.dequeueReusableCell(withIdentifier: "invitesCell", for: indexPath)
-            cell.textLabel?.text = Invite.to
+            cell.textLabel?.text = Invite.from
             cell.textLabel?.textColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
             cell.backgroundColor = .clear
             return cell
@@ -37,6 +57,33 @@ class InvitesPendingVC: UIViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(invites)
         dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+extension InvitesPendingVC {
+    
+    private func addListener() {
+        inviteListener = collectionReference.whereField(
+            "to",
+            isEqualTo: currentUserEmail
+        )
+            .addSnapshotListener({ (snapshot, error) in
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                guard let invitesFromOnline = snapshot?.documents else {
+                    print("no invites available")
+                    return
+                }
+                let inviteList = invitesFromOnline.compactMap { (snapshot) -> Invites? in
+                    let inviteID = snapshot.documentID
+                    let data = snapshot.data()
+                    return Invites(from: data, id: inviteID)
+                }
+                self.invites = inviteList
+                print("inviteList: \(inviteList)")
+            })
     }
 }
 
