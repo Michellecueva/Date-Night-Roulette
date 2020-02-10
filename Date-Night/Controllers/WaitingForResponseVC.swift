df30f54740cea96fed790a33887c99ca52038678
Kyang646 <kimballyang@pursuit>
2020-02-10 14:42:41 -0500

    WaitingForResponse VC added

---
+++ Date-Night/Controllers/WaitingForResponseVC.swift
@@ -0,0 +1,25 @@
+//
+//  WaitingForResponseVC.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 2/10/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class WaitingForResponseVC: UIViewController {
+    
+var waitingResponse = WaitingForResponseView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(waitingResponse)
+        
+        
+    }
+    
+
+    
+
+}
