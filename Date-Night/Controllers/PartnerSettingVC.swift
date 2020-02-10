989204eecb461da455d22846784489b7e8099cb9
Kyang646 <kimballyang@pursuit>
2020-02-10 12:09:59 -0500

    PartnerSettingVC skeleton + username

--- Date-Night/Controllers/PartnerSettingVC.swift
+++ Date-Night/Controllers/PartnerSettingVC.swift
@@ -1,90 +1,32 @@
-//
-//  PartnerSettingVC.swift
-//  Date-Night
-//
-//  Created by Kimball Yang on 2/10/20.
-//  Copyright © 2020 Michelle Cueva. All rights reserved.
-//
-
 import UIKit
 
-class PartnerSettingVC: UITableViewController {
+class PartnerSettingVC: UIViewController {
 
+    var thePartner = PartnerSettingView()
+    var dummyArray = ["Hiking", "Concert", "Comedy club", "Biking", "Spelunking"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.addSubview(thePartner)
+        thePartner.historyTable.delegate = self
+        thePartner.historyTable.dataSource = self
     }
+    
+}
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+extension PartnerSettingVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyArray.count
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = thePartner.historyTable.dequeueReusableCell(withIdentifier: "historyCell", for: indexPath)
+        cell.backgroundColor = #colorLiteral(red: 0.9092509151, green: 0.7310814261, blue: 1, alpha: 1)
+        cell.textLabel?.text = dummyArray[indexPath.row]
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
--- Date-Night/Views/PartnerSettingView.swift
+++ Date-Night/Views/PartnerSettingView.swift
@@ -1,21 +1,119 @@
-//
-//  PartnerSettingView.swift
-//  Date-Night
-//
-//  Created by Kimball Yang on 2/10/20.
-//  Copyright © 2020 Michelle Cueva. All rights reserved.
-//
-
 import UIKit
 
 class PartnerSettingView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+   lazy var portraitPic: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "person.crop.circle.badge.plus")
+        return image
+    }()
+    
+    lazy var partnerNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Partner Username"
+        label.font = UIFont(name: "Palatino-bold", size: 25)
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = #colorLiteral(red: 0.9092509151, green: 0.7310814261, blue: 1, alpha: 1)
+        return label
+    }()
+    
+    lazy var historyTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "historyCell")
+        table.backgroundColor = #colorLiteral(red: 0.9092509151, green: 0.7310814261, blue: 1, alpha: 1)
+        return table
+    }()
+    
+    lazy var removePartnerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Remove Partner", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.9092509151, green: 0.7310814261, blue: 1, alpha: 1)
+        button.titleLabel?.font = UIFont(name: "Arial", size: 20)
+        button.layer.cornerRadius = 5
+        button.isEnabled = true
+        return button
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        self.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        setSubviews()
+        setConstraints()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setSubviews() {
+        self.addSubview(portraitPic)
+        self.addSubview(partnerNameLabel)
+        self.addSubview(historyTable)
+        self.addSubview(removePartnerButton)
+    }
+    
+    
+    
+    private func setConstraints() {
+        setPortraitConstraints()
+        setPartnerNameLabelConstraints()
+        setTableConstraints()
+        setRemoveButtonConstraints()
+    }
+    
+    private func setPortraitConstraints() {
+        portraitPic.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            portraitPic.topAnchor.constraint(equalTo: self.topAnchor, constant: self.frame.height * 0.1),
+            portraitPic.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            portraitPic.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
+            portraitPic.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.4)
+        ])
+    }
+    
+    private func setPartnerNameLabelConstraints() {
+        partnerNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            partnerNameLabel.topAnchor.constraint(equalTo: portraitPic.bottomAnchor, constant: 10),
+            partnerNameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            partnerNameLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.6),
+            partnerNameLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    private func setUserEmailLabelConstraints() {
+        partnerNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            partnerNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: self.frame.height * 0.45),
+            partnerNameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -50),
+            partnerNameLabel.widthAnchor.constraint(equalToConstant: 200),
+            partnerNameLabel.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func setTableConstraints() {
+        historyTable.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            historyTable.topAnchor.constraint(equalTo: portraitPic.bottomAnchor, constant: 70),
+            historyTable.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 70),
+            historyTable.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -70),
+            historyTable.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.4)
+        ])
+    }
+    
+    private func setRemoveButtonConstraints() {
+        removePartnerButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            removePartnerButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50),
+            removePartnerButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            removePartnerButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.44),
+            removePartnerButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+    
+    
 }
+
