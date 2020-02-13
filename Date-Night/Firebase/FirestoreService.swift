ad579058b229aecd247fcd6934fff05a40fd4537
Michelle <Michelle.cueva@gmail.com>
2020-02-13 17:48:04 -0500

    Creates function to get partners user object

--- Date-Night/Firebase/FirestoreService.swift
+++ Date-Night/Firebase/FirestoreService.swift
@@ -14,9 +14,9 @@ enum FireStoreCollections: String {
 }
 
 enum InviteField:String {
-  case to
-  case from
-  case invitationStatus
+    case to
+    case from
+    case invitationStatus
 }
 
 enum SortingCriteria: String {
@@ -58,13 +58,13 @@ class FirestoreService {
         if let firstName = firstName {
             updateFields["firstName"] = firstName
         }
-       
+        
         
         if let photo = photoURL {
             updateFields["photoURL"] = photo.absoluteString
         }
         
-       
+        
         //PUT request
         db.collection(FireStoreCollections.users.rawValue).document(userId).updateData(updateFields) { (error) in
             if let error = error {
@@ -82,7 +82,7 @@ class FirestoreService {
             return
         }
         var updateFields = [String:Any]()
-    
+        
         if let partnerEmail = partnerEmail {
             updateFields["partnerEmail"] = partnerEmail
         }
@@ -112,7 +112,7 @@ class FirestoreService {
             }
         }
     }
-
+    
     func deleteAllDocuments(docIDs:[String],path:FireStoreCollections,completionHandler:@escaping(Result<(),AppError>) -> ()) {
         
         for docID in docIDs {
@@ -125,64 +125,82 @@ class FirestoreService {
             }
         }
     }
-
-   
+    
+    
     func getUser(userID: String, completion: @escaping (Result<AppUser, Error>) -> ()) {
-    db.collection(FireStoreCollections.users.rawValue).document(userID).getDocument { (snapshot, error) in
-        if let error = error {
-            completion(.failure(error))
-        } else {
-            let userdata = snapshot?.data()
-            let user = AppUser(from: userdata!, id: userID)
-            completion(.success(user!))
+        db.collection(FireStoreCollections.users.rawValue).document(userID).getDocument { (snapshot, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let userdata = snapshot?.data()
+                let user = AppUser(from: userdata!, id: userID)
+                completion(.success(user!))
+            }
         }
     }
+    
+    func getPartnersUserData(partnerEmailAddress: String, completionHandler: @escaping (Result<[AppUser], Error>) -> ()) {
+        db.collection(FireStoreCollections.users.rawValue).whereField("email", isEqualTo: partnerEmailAddress.lowercased()).getDocuments { (snapshot, error) in
+            if let error = error {
+                completionHandler(.failure(error))
+            } else {
+                let userData = snapshot?.documents.compactMap({ (snapshot) -> AppUser? in
+                    
+                    let userID = snapshot.documentID
+                    let data = snapshot.data()
+                    return AppUser(from: data, id: userID)
+                })
+                completionHandler(.success(userData ?? []))
+            }
+        }
     }
     
-    func sendInvite(invite:Invites,completionHandler:@escaping (Result<(),AppError>)-> ()) {
-          let inviteField = invite.fieldsDictionary
-          db.collection(FireStoreCollections.invites.rawValue).addDocument(data: inviteField) { (error) in
-              if let error = error {
-                  completionHandler(.failure(.other(rawError: error)))
-              } else {
-                  completionHandler(.success(()))
-              }
-          }
-          //add snapshot listener
-      }
-       
-//  messageListener = reference?.addSnapshotListener { querySnapshot, error in
-//    guard let snapshot = querySnapshot else {
-//      print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
-//      return
-//    }
-//
-//    snapshot.documentChanges.forEach { change in
-//      self.handleDocumentChange(change)
-//    }
-//  }
-//
-   
-// MARK: Invitation Functionality
     
-
-      func getAllInvites(userEmailAddress:String,completionHandler:@escaping (Result<[Invites],AppError>)-> ()) {
-         
-          
-          db.collection(FireStoreCollections.invites.rawValue).whereField("to", isEqualTo: userEmailAddress.lowercased()).getDocuments { (snapshot, error) in
-                 if let error = error {
-                     completionHandler(.failure(.other(rawError: error)))
-                 } else {
-                     let inviteData = snapshot?.documents.compactMap({ (snapshot) -> Invites? in
-                        
-                         let inviteID = snapshot.documentID
-                         let data = snapshot.data()
-                         return Invites(from: data, id: inviteID)
-                     })
-                     completionHandler(.success(inviteData ?? []))
-                 }
-             }
-         }
+    
+    func sendInvite(invite:Invites,completionHandler:@escaping (Result<(),AppError>)-> ()) {
+        let inviteField = invite.fieldsDictionary
+        db.collection(FireStoreCollections.invites.rawValue).addDocument(data: inviteField) { (error) in
+            if let error = error {
+                completionHandler(.failure(.other(rawError: error)))
+            } else {
+                completionHandler(.success(()))
+            }
+        }
+        //add snapshot listener
+    }
+    
+    //  messageListener = reference?.addSnapshotListener { querySnapshot, error in
+    //    guard let snapshot = querySnapshot else {
+    //      print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
+    //      return
+    //    }
+    //
+    //    snapshot.documentChanges.forEach { change in
+    //      self.handleDocumentChange(change)
+    //    }
+    //  }
+    //
+    
+    // MARK: Invitation Functionality
+    
+    
+    func getAllInvites(userEmailAddress:String,completionHandler:@escaping (Result<[Invites],AppError>)-> ()) {
+        
+        
+        db.collection(FireStoreCollections.invites.rawValue).whereField("to", isEqualTo: userEmailAddress.lowercased()).getDocuments { (snapshot, error) in
+            if let error = error {
+                completionHandler(.failure(.other(rawError: error)))
+            } else {
+                let inviteData = snapshot?.documents.compactMap({ (snapshot) -> Invites? in
+                    
+                    let inviteID = snapshot.documentID
+                    let data = snapshot.data()
+                    return Invites(from: data, id: inviteID)
+                })
+                completionHandler(.success(inviteData ?? []))
+            }
+        }
+    }
     
     
     func removeInvite(invite:Invites, completion: @escaping (Result<(), Error>) -> ()) {
