086eebbd0e44e4e5ea0fb3486a673db437da041b
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-13 15:09:06 -0500

    creates new protocol for appNavigation

---
+++ Date-Night/AppNavigationEnums/Enums.swift
@@ -0,0 +1,17 @@
+//
+//  Enums.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/13/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import Foundation
+
+enum HomePageStatus:String {
+    case pendingInvites
+    case discoverEvents
+    case setPreferences
+}
+
+
