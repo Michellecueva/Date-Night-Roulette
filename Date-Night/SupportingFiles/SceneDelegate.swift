f174cf90542bceeb8237af21af18eadce65dcc12
Kyang646 <kimballyang@pursuit>
2020-02-14 14:54:16 -0500

    Add SplashScreen and transition

--- Date-Night/SupportingFiles/SceneDelegate.swift
+++ Date-Night/SupportingFiles/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = scene
-        window?.rootViewController = ProfileSettingVC()
+        window?.rootViewController = SplashScreenVC()
         
         window?.makeKeyAndVisible()
     }
--- Date-Night/Views/SplashScreenView.swift
+++ Date-Night/Views/SplashScreenView.swift
@@ -10,12 +10,47 @@ import UIKit
 
 class SplashScreenView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    lazy var splashLogo: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "splashLogo.png")
+        return image
+    }()
+    
+    private func rotateSplashLogo() {
+    UIView.animate(withDuration: 2.0, animations: {
+        self.splashLogo.transform = CGAffineTransform(rotationAngle: (180.0 * .pi) / 180.0)
+            }
+        )
 
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        self.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        setSubviews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    private func setSubviews() {
+        self.addSubview(splashLogo)
+    }
+    
+    private func setConstraints() {
+        splashLogo.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            splashLogo.topAnchor.constraint(equalTo: self.topAnchor, constant: self.frame.height * 0.3),
+            splashLogo.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8),
+            splashLogo.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8),
+            splashLogo.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+        
+        
+    }
+    
+    
 }
