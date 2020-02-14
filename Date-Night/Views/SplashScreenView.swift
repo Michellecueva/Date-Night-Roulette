32c02ceafc3ecd8a4fb400bafcf3bf0294aaf542
Kyang646 <kimballyang@pursuit>
2020-02-14 14:03:29 -0500

    first

---
+++ Date-Night/Views/SplashScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  SplashScreenView.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 2/14/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
