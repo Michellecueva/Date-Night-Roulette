06a188ca346e03e3d4103d75e8c944dd4a8d3ac6
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-14 12:53:22 -0500

    xcode projc

---
+++ Date-Night/Protocols/WaitingForResponseDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  WaitingForResponseDelegate.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/13/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import Foundation
+
+protocol WaitingForResponseDelegate:AnyObject {
+    func  changeStatus(status: LeftScreenStatus)
+}
