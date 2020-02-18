95bec5cf8541cba9362314c06afef7f0b3486a84
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-18 14:58:45 -0500

    adds xcode proj file

---
+++ Date-Night/Controllers/ShowEventVC.swift
@@ -0,0 +1,30 @@
+//
+//  ShowEventViewController.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/18/20.
+//  Copyright © 2020 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class ShowEventVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
