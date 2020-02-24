9f53079a507fbee050b96f764687fdaee119b633
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-24 14:47:40 -0500

    creates adminVC

--- Date-Night/Controllers/AdminViewController.swift
+++ Date-Night/Controllers/AdminViewController.swift
@@ -8,23 +8,67 @@
 
 import UIKit
 
-class AdminViewController: UIViewController {
 
+class AdminViewController: UIViewController {
+    
+    var arrayOfEvents = [Event]() {
+         didSet {
+             print("added Event  #\(arrayOfEvents.count)")
+         }
+     }
+     var preferenceArray:[String] {
+         guard let preferences =  UserDefaultsWrapper.standard.getPreferences() else {fatalError()}
+         return preferences
+     }
+    
+
+    let adminView = AdminView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .clear
+        view.addSubview(adminView)
+        addTargetToButton()
+        
         // Do any additional setup after loading the view.
     }
     
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func addEventsToFirebase() {
+     addEventsFromAPI()
     }
-    */
-
+    
+    private func addTargetToButton() {
+        adminView.addEventsButton.addTarget(self, action: #selector(addEventsToFirebase), for: .touchUpInside)
+    }
+        private func addEventsFromAPI() {
+            for preference in preferenceArray {
+                EventfulAPIClient.shared.getEventsFrom(category: preference) { (result) in
+                    switch result {
+                    case .failure(let error):
+                        print(error)
+                    case .success(let event):
+                        self.arrayOfEvents += event
+                        self.sendEventToFireBase(preference: preference, eventArray: event)
+                    }
+                }
+            }
+        }
+        private func sendEventToFireBase(preference:String,eventArray:[Event]) {
+    
+            for event in eventArray {
+    
+                let fbEvent = FBEvents(title: event.title, address: event.venueAddress, eventID: String(event.id), description: event.eventDescription, imageURL: nil, websiteURL: event.url,type:preference)
+    
+                FirestoreService.manager.sendEventsToFirebase(event: fbEvent) { (result) in
+                    switch result {
+                    case .failure(let error):
+                        print(error)
+                    case .success():
+                        print("added Event to firebase")
+                    }
+                }
+        }
+}
 }
