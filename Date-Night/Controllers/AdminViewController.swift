4bdafb99635cf9c77e799f7f62549f3382078e64
phoenixmcknight <phoenixmcknight@gmail.com>
2020-02-24 14:47:12 -0500

    xcode proj

---
+++ Date-Night/Controllers/AdminViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminViewController.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/23/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Date-Night/Views/adminView.swift
@@ -0,0 +1,21 @@
+//
+//  adminView.swift
+//  Date-Night
+//
+//  Created by Phoenix McKnight on 2/23/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class adminView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
