2683b36097be12044cf65b8e46692116a77cf0b2
Krystal Campbell <krystalcampbell@Krystals-MBP.home>
2020-02-28 11:59:31 -0500

    Adds new file for matched cell for Events History tableview, adds new file for Matched History Model

---
+++ Date-Night/Models/MatchedHistory.swift
@@ -0,0 +1,9 @@
+//
+//  MatchedHistory.swift
+//  Date-Night
+//
+//  Created by Krystal Campbell on 2/27/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import Foundation
---
+++ Date-Night/Views/MatchedCell.swift
@@ -0,0 +1,24 @@
+//
+//  MatchedCell.swift
+//  Date-Night
+//
+//  Created by Krystal Campbell on 2/27/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class MatchedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
