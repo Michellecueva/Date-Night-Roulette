e737758f00663581c7eab4a13269d6b011a5ef88
Kyang646 <kimballyang@pursuit>
2020-03-05 15:09:10 -0500

    first

---
+++ Date-Night/Controllers/MatchedEventVC.swift
@@ -0,0 +1,30 @@
+//
+//  MatchedEventVC.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 3/3/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class MatchedEventVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Date-Night/Views/MatchedEventView.swift
@@ -0,0 +1,21 @@
+//
+//  MatchedEventView.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 3/3/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class MatchedEventView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
