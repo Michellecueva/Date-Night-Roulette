aecda409564f76b9df878e6f2a4729e475a4fc57
Kyang646 <kimballyang@pursuit>
2020-03-06 16:58:59 -0500

    MatchedEvent WIP but not errors

---
+++ Date-Night/Controllers/MatchedInfoVC.swift
@@ -0,0 +1,38 @@
+//
+//  MatchedInfoVC.swift
+//  Date-Night
+//
+//  Created by Kimball Yang on 3/6/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class MatchedInfoVC: UIViewController {
+
+    var matchedInfo = MatchedInfoView()
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(matchedInfo)
+        
+        
+        
+
+    }
+    
+    @objc func showInfoText() {
+        // TODO: this function will present the detailed event information
+        
+    }
+    
+
+    
+    
+    
+}
