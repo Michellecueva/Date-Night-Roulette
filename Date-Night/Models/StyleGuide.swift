a137920c4b606ec968f943f5cd04e7bd1912c8af
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2020-03-09 11:07:42 -0400

    Set up enums to standardize font styles, button styles to start

--- Date-Night/Models/StyleGuide.swift
+++ Date-Night/Models/StyleGuide.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+enum StyleGuide {
+   
+  enum TitleFontStyle{
+     static let fontName = "CopperPlate"
+    }
+  enum FontStyle {
+    static let fontName = "Helvetica-Neue"
+  }
+  enum AppColors{
+    static let primaryColor: UIColor = .gray
+  }
+  enum ImageStrings {
+    static let placeHolder = "photo"
+  }
+   
+//  enum ButtonStyle {
+//    static let
+//  }
+//  enum TitleFontStyle{
+//    static let
+//  }
+//
+//
+}
