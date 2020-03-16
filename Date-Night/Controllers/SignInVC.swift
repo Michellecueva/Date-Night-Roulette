ab1dfa59b13866b336c2f9a1ca6421464a37c213
Michelle <Michelle.cueva@gmail.com>
2020-03-16 14:44:20 -0400

    Dismisses the keyboard when you hit return and adds view into scrollview

--- Date-Night/Controllers/SignInVC.swift
+++ Date-Night/Controllers/SignInVC.swift
@@ -5,22 +5,85 @@ class SignInVC: UIViewController {
     
     let viewSignIn = SignInView()
     
+    var scrollView = UIScrollView(frame: UIScreen.main.bounds)
+    
+    lazy var bottomScrollConstraint: NSLayoutConstraint = {
+           scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    }()
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(viewSignIn)
+        view.addSubview(scrollView)
         addObjcFunctionsToViewObjects()
+        addKeyboardAppearObserver()
+        addKeyboardDismissObserver()
+        setScrollView()
+        setDelegate()
+    }
+    
+    override func viewWillLayoutSubviews() {
+          super.viewWillLayoutSubviews()
+          setUpScrollView()
+      }
+    
+    private func setDelegate() {
+        viewSignIn.emailField.delegate = self
+        viewSignIn.passwordField.delegate = self
+    }
+    
+    private func setScrollView() {
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.scrollView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20),
+            self.scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            bottomScrollConstraint
+        ])
+    }
+    
+    private func addKeyboardAppearObserver()
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardAppearing(sender:)), name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+    }
+    
+    private func addKeyboardDismissObserver()
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardDisappearing(sender:)), name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    private func adjustInsetForKeyboardShow(_ show: Bool, notification: Notification) {
+          guard
+            let userInfo = notification.userInfo,
+            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey]
+              as? NSValue
+            else {
+              return
+          }
+            
+          let adjustmentHeight = (keyboardFrame.cgRectValue.height + 20) * (show ? 3 : -3)
+          scrollView.contentInset.bottom += adjustmentHeight
+          scrollView.verticalScrollIndicatorInsets.bottom += adjustmentHeight
+    }
+    
+    @objc func handleKeyboardAppearing(sender: Notification) {
+        adjustInsetForKeyboardShow(true, notification: sender)
+    }
+    
+    @objc func handleKeyboardDisappearing(sender: Notification) {
+        adjustInsetForKeyboardShow(false, notification: sender)
+        
     }
     
     @objc private func loginButton() {
         loginButtonFunctions(email:viewSignIn.emailField.text,password:viewSignIn.passwordField.text)
-        
     }
     
     @objc private func createAccountButton() {
         createAccount()
     }
-    
-    
     
     private func addObjcFunctionsToViewObjects() {
         viewSignIn.loginButton.addTarget(self, action: #selector(loginButton), for: .touchUpInside)
@@ -29,8 +92,8 @@ class SignInVC: UIViewController {
     
     private func createAccount() {
         let signupVC = SignUpVC()
-             signupVC.modalPresentationStyle = .formSheet
-             present(signupVC, animated: true, completion: nil)
+        signupVC.modalPresentationStyle = .formSheet
+        present(signupVC, animated: true, completion: nil)
     }
     
     private func loginButtonFunctions(email:String?,password:String?) {
@@ -75,5 +138,29 @@ class SignInVC: UIViewController {
             //add alert
         }
     }
+    
+    private func setUpScrollView() {
+        scrollView.addSubview(viewSignIn)
+        scrollView.alwaysBounceVertical = false
+        view.addSubview(scrollView)
+        scrollView.delegate = self
+    }
 }
+
+extension SignInVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension SignInVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("Did scroll")
+    }
+}
+
+
+
+
 
