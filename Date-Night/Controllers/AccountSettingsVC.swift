d2f3d298eddeebd955988b5295570928848f31f1
kcamp92 <krystalcampbell@pursuit.org>
2020-03-24 17:19:41 -0400

    Adds AccSettings View to its View Controller

--- Date-Night/Controllers/AccountSettingsVC.swift
+++ Date-Night/Controllers/AccountSettingsVC.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class AccountSettingsVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
+    let accSettings = AccountSettingsView()
+      
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          view.addSubview(accSettings)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+      }
+    
 
 }
