a9f5ca5c6a6563be6fedd19786a543fe6605c6f3
kcamp92 <krystalcampbell@pursuit.org>
2020-03-24 19:23:19 -0400

    Adds objc methods for buttons and adds subview to controller

--- Date-Night/Controllers/AccountSettingsVC.swift
+++ Date-Night/Controllers/AccountSettingsVC.swift
@@ -7,18 +7,105 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class AccountSettingsVC: UIViewController {
-
     
     let accSettings = AccountSettingsView()
-      
-      
-      override func viewDidLoad() {
-          super.viewDidLoad()
-          view.addSubview(accSettings)
-
-      }
     
-
+    private var isListenerEnabled:Bool = false
+    
+    var profilePartnerUser:AppUser? {
+        didSet {
+            isListenerEnabled = true
+        }
+        
+    }
+    var currentUser:AppUser? = nil {
+        didSet {
+            print("profile setting vc received current User")
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(accSettings)
+        addObjcFunctions()
+    }
+    
+    private func addObjcFunctions() {
+        accSettings.displayNameButton.addTarget(self, action: #selector(changeDisplayName), for: .touchUpInside)
+        accSettings.removePartnerButton.addTarget(self, action: #selector(removePartner), for: .touchUpInside)
+        accSettings.helpButton.addTarget(self, action: #selector(getSupport), for: .touchUpInside)
+        accSettings.notificationsButton.addTarget(self, action: #selector(toggleNotifications), for: .touchUpInside)
+        accSettings.logoutButton.addTarget(self, action: #selector(logOut), for: .touchUpInside)
+    }
+    
+    
+    @objc private func changeDisplayName(){
+          //  present(OBDisplayNameSetupVC(), animated: true, completion: nil)
+    }
+    
+    @objc private func removePartner(){
+        
+        FirestoreService.manager.deleteMatchedEvents(coupleID: profilePartnerUser?.coupleID) { [weak self](result) in
+            self?.handlePartnerRemoval(result: result)
+        }
+        FirestoreService.manager.removePartnerReferencesInUserCollection(uid: Auth.auth().currentUser?.uid, partnerUID: profilePartnerUser?.uid) { [weak self](result) in
+            self?.handlePartnerRemoval(result: result)
+        }
+    }
+    
+    private func handlePartnerRemoval(result:Result<(),AppError>) {
+        switch result {
+        case .failure(let error):
+            print(error)
+        case .success():
+            print("successfully removed partner reference")
+        }
+    }
+    
+    @objc private func getSupport(){}
+    
+    @objc private func toggleNotifications(){
+     //present(OBNotifcationsVC(), animated: true, completion: nil)
+    }
+    
+    @objc private func logOut(){
+        FirebaseAuthService.manager.signOut { [weak self](result) in
+            self?.handleSignOut(vc: SignInVC(), with: result)
+        }
+        
+    }
+    
+    private func handleSignOut(vc
+        viewController:UIViewController,with result: Result<(), AppError>) {
+        switch result {
+            
+        case .success:
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let sceneDelegate = windowScene.delegate as? SceneDelegate, let window = sceneDelegate.window
+                else {
+                    return
+            }
+            
+            UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromBottom, animations: {
+                if FirebaseAuthService.manager.currentUser == nil {
+                    window.rootViewController = viewController
+                    
+                } else {
+                    
+                    print("No current user")
+                }
+            }, completion: nil)
+        case .failure(let error):
+            print(error)
+        }
+    }
 }
+
+
+
