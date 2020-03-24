0088728a69b92177f6fec5666883d0016fb8976a
kcamp92 <krystalcampbell@pursuit.org>
2020-03-24 15:20:02 -0400

    Add corresponding view as a subview to respective View Controllers

--- Date-Night/Controllers/OBLocationServicesVC.swift
+++ Date-Night/Controllers/OBLocationServicesVC.swift
@@ -10,21 +10,32 @@ import UIKit
 
 class OBLocationServicesVC: UIViewController {
 
+     let obLocations = OBLocationServicesView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(obLocations)
+       // setupScene()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    private func setupScene(){
+//
+//           self.navigationController?.navigationBar.topItem?.title = "Date Night Roulette"
+//           navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Skip", style: .plain, target: self, action: #selector(skipButton))
+//           self.navigationController?.navigationBar.barTintColor = .darkGray
+//           navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.9712318778, green: 0.9606906772, blue: 0.6410447955, alpha: 1)
+//           navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.init(red: 0.9712318778, green: 0.9606906772, blue: 0.6410447955, alpha: 1)]
+//       }
+//
+//
+//    @objc private func skipButton(){
+//        present(OBNotifcationsVC(), animated: true, completion: nil)
+//
+//    }
+    
+    
 
 }
--- Date-Night/Controllers/OBNotifcationsVC.swift
+++ Date-Night/Controllers/OBNotifcationsVC.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class OBNotifcationsVC: UIViewController {
-
+    
+    
+  let obNotifications = OBNotificationsView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(obNotifications)
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
