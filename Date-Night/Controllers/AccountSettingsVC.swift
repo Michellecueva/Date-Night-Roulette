8426de8e187206437ed1c77154c960be69d7b26e
kcamp92 <krystalcampbell@pursuit.org>
2020-03-24 17:17:52 -0400

    Creates new files for the account settings portion of the app

---
+++ Date-Night/Controllers/AccountSettingsVC.swift
@@ -0,0 +1,30 @@
+//
+//  AccountSettingsVC.swift
+//  Date-Night
+//
+//  Created by Krystal Campbell on 3/24/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class AccountSettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
