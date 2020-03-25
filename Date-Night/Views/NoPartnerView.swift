2043682f2beacdc81caae4f38bb268a0eb575e07
kcamp92 <krystalcampbell@pursuit.org>
2020-03-25 13:25:24 -0400

    Adds new image to asset for noPartner view, creates new files for no partner view and view controllers

---
+++ Date-Night/Views/NoPartnerView.swift
@@ -0,0 +1,21 @@
+//
+//  NoPartnerView.swift
+//  Date-Night
+//
+//  Created by Krystal Campbell on 3/25/20.
+//  Copyright © 2020 Date Night Roulette. All rights reserved.
+//
+
+import UIKit
+
+class NoPartnerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
