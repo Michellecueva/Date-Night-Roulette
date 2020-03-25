5ebbe9fe952b9dc57e7a2b668ccd12ab2ca519f9
kcamp92 <krystalcampbell@pursuit.org>
2020-03-25 13:29:02 -0400

    Adds noPartner view to view controller and adds subview to viewDidLoad function

--- Date-Night/Controllers/NoPartnerVC.swift
+++ Date-Night/Controllers/NoPartnerVC.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class NoPartnerVC: UIViewController {
-
+ 
+    let noPartner = NoPartnerView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(noPartner)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
